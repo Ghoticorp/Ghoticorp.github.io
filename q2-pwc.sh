716636375d1121e6dd13088dffc018470853eedd
Ghoticorp <demi1@ghoticorp.com>
2024-07-04 15:48:14 +0100

    Delete q2-pwc.sh

--- q2-pwc.sh
+++
@@ -1,8 +0,0 @@
-VALID_PASSWORD="IMHUNGRY"
-echo "Enter the password:"
-read entered_password
-if [ "$entered_password" == "$VALID_PASSWORD" ]; then
-	echo "You have Access"
-else
-	echo "Access Denied"
-fi
